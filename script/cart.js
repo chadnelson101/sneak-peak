308376cc104108c41c563b6e4d544cbb384bca20
chadnelson101 <chadnelson279@gmail.com>
2023-12-08 11:13:02 +0200

    neww

--- script/cart.js
+++ script/cart.js
@@ -3,32 +3,10 @@ let main = document.querySelector('main');
 // Load existing items from local storage or initialize an empty array
 let cart = JSON.parse(localStorage.getItem('product')) || [];
 
-// Assuming you have a function to handle the purchase and updates the cart
-// This is just a simplified example; replace it with your actual logic
-function purchaseProduct(index) {
-    cart[index].quantity += 1;
-    updateLocalStorage();
-    inputDisplay(cart[index]);
-}
-
-// Function to display quantity and total for a product
-function inputDisplay(product) {
-    let quantityInput = document.getElementById(`quantity-amount${index}`);
-    let totalInput = document.getElementById(`total-amount${index}`);
-
-    // Calculate total for the current product
-    let productTotal = product.price * product.quantity;
-
-    // Display quantity and total in their respective inputs
-    quantityInput.value = quantity-amount;
-    totalInput.value.innerHTML = productTotal;
-
-    // You can update other parts of your UI as needed
-}
 
 main.innerHTML = cart.map(function(item, index) {
     return `
-        <table class="table">
+        <table class="table"z>
             <thead>
                 <tr>
                     <th scope="col">#</th>
